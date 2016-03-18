dcad47158accc5d7343e87ce68d69da5473939e4
Joel Dillon <joelrdillon@gmail.com>
2016-03-18 13:08:35 +0000

    Add some load commands

--- macho.cpp
+++ macho.cpp
@@ -79,6 +79,78 @@ void MachOImage::finalise()
     {
         wee32(le, ptr, 0x0); // Reserved
     }
+
+    for (int loopc=0; loopc<4; loopc++)
+    {
+        wee32(le, ptr, sf_bit ? 0x19 : 0x1);  // command type
+        wee32(le, ptr, sf_bit ? 72 : 56);     // command size
+	char buf[16];
+	int prot = 0;
+	if (loopc == IMAGE_CODE)
+	{
+  	    strcpy(buf, "__TEXT");  
+	    prot = 0x5;
+        }
+	else if (loopc == IMAGE_DATA)
+	{
+  	    strcpy(buf, "__DATA");
+            prot = 0x3;
+	}
+	else if (loopc == IMAGE_CONST_DATA)
+        {
+  	    strcpy(buf, "__RDATA");
+	    prot = 0x1;
+	}
+	else if (loopc == IMAGE_UNALLOCED_DATA)
+	{
+  	    strcpy(buf, "__BSS");
+	    prot = 0x3;
+	}
+	strcpy((char *)ptr, buf);
+	ptr += 16;
+	if (sf_bit)
+	{
+  	    wee64(le, ptr, bases[loopc]);
+	    wee64(le, ptr, sizes[loopc]);
+	    wee64(le, ptr, bases[loopc]-base_addr);
+	    wee64(le, ptr, (loopc == IMAGE_UNALLOCED_DATA) ? 0 : sizes[loopc]);
+        }
+	else
+        {
+  	    wee32(le, ptr, bases[loopc]);
+	    wee32(le, ptr, sizes[loopc]);
+	    wee32(le, ptr, bases[loopc]-base_addr);
+	    wee32(le, ptr, (loopc == IMAGE_UNALLOCED_DATA) ? 0 : sizes[loopc]);
+	}
+	wee32(le, ptr, prot);   // maxprot
+	wee32(le, ptr, prot);   // initprot
+	wee32(le, ptr, 0);      // nsects
+	wee32(le, ptr, 0x4);    // flags - no reloc
+    }
+
+    // note this is x86-specific right now!
+    if (sf_bit)
+    {
+        wee32(le, ptr, 0x5);
+        wee32(le, ptr, 184);
+	wee32(le, ptr, 0x4);
+	wee32(le, ptr, 42);
+	for (int loopc=0; loopc<42; loopc++)
+        {
+  	    wee64(le, ptr, loopc == 16 ? functionAddress(root_function) : 0);
+        }
+    }
+    else
+    {
+        wee32(le, ptr, 0x5);   // LC_UNIXTHREAD
+        wee32(le, ptr, 80);    // size
+        wee32(le, ptr, 1);     // flavour
+        wee32(le, ptr, 16);    // count
+        for (int loopc=0; loopc<16; loopc++)
+        {
+  	    wee32(le, ptr, loopc == 10 ? functionAddress(root_function) : 0);
+        }
+    }
     
     fwrite(header, 4096, 1, f);
     delete[] header;
