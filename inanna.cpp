f77cd5cf3db1d29f3d1f9c2ba506219e7918f1bc
Joel Dillon <joelrdillon@gmail.com>
2018-06-11 18:42:59 +0100

    Skeleton header

--- inanna.cpp
+++ inanna.cpp
@@ -1,4 +1,13 @@
 #include "inanna.h"
+#include "platform.h"
+#include "symbols.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#if defined(POSIX_HOST)
+#include <sys/stat.h>
+#endif
 
 InannaImage::InannaImage()
 {
@@ -11,6 +20,24 @@ InannaImage::~InannaImage()
 
 void InannaImage::finalise()
 {
+    FILE * f = fopen(fname.c_str(), "wb+");
+    if (!f)
+    {
+        printf("Can't open %s\n", fname.c_str());
+        return;
+    }
+
+    unsigned char * header = new unsigned char[4096];
+    memset((char *)header, 0, 4096);
+    strcpy((char *)header, "/usr/bin/env enki\n");
+    strcpy((char *)header + 512, "enki");
+
+    fwrite(header, 4096, 1, f);
+
+    fclose(f);
+#if defined(POSIX_HOST)
+    chmod(fname.c_str(), 0755);
+#endif
 }
 
 bool InannaImage::configure(std::string param, std::string val)
