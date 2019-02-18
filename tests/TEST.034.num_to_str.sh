ab98ed610b50515b98a65acc63f1cb0771befe61
Joel Dillon <joelrdillon@gmail.com>
2019-02-18 18:34:33 +0000

    New test output for 32 bit systems

--- tests/TEST.034.num_to_str.sh
+++ tests/TEST.034.num_to_str.sh
@@ -5,7 +5,7 @@ rm -f $OUTPUT
 if [[ `uname -m` == "armv7l" ]]; then
     compile TEST.034.num_to_str.e
     STDOUT=`$OUTPUT`
-    expectResult 16 "$STDOUT" "        deadbeef"
+    expectResult 16 "$STDOUT" "00000000deadbeef"
 else
     compile TEST.034.num_to_str.e
     STDOUT=`$OUTPUT`
